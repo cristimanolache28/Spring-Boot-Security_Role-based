ac06a69d80b87ba03bf3cdbbf9966550fd582c5f
Cristian Manolache <cristianmanolache0@gmail.com>
2022-04-13 00:47:57 +0300

    configure Spring Security Authentication & Authorization

---
+++ src/main/java/com/lov2code/security/UserDetailsServiceImpl.java
@@ -0,0 +1,24 @@
+package com.lov2code.security;
+
+import com.lov2code.model.User;
+import com.lov2code.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.getUserByUsername(username);
+
+        if (user == null) {
+            throw new UsernameNotFoundException("Could not find user.");
+        }
+        return new CustomUserDetails(user);
+    }
+}
