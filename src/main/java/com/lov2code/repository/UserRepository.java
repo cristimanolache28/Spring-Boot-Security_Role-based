b257c55dfb4cf42e4cd3dc70604ab12d924ceaff
Cristian Manolache <cristianmanolache0@gmail.com>
2022-04-13 00:45:03 +0300

    adding UserRepository

--- src/main/java/com/lov2code/repository/UserRepository.java
+++ src/main/java/com/lov2code/repository/UserRepository.java
@@ -1,4 +1,15 @@
 package com.lov2code.repository;
 
-public class UserRepository {
+import com.lov2code.model.User;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+
+    // method used by Spring Security for authentication
+    @Query("SELECT u FROM User u WHERE u.username = :username")
+    public User getUserByUsername(@Param("username") String username);
 }
